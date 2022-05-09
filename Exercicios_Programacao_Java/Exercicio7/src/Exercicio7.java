9684bce7f19d7e136f77a01c6e2f6555fef1d144
vxpresto <nallinguilherme@gmail.com>
2022-05-09 17:14:06 -0300

    add ex8

--- Exercicios_Programacao_Java/Exercicio7/src/Exercicio7.java
+++ Exercicios_Programacao_Java/Exercicio7/src/Exercicio7.java
@@ -1,11 +1,12 @@
-import java.awt.*; import javax.swing.*;
+import java.awt.*; 
+import javax.swing.*;
 
 public class Exercicio7 {
 	private static void createWindow() {
 		JFrame frame = new JFrame("Simple Window");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER); 
+		JLabel textLabel = new JLabel("Sou uma janela !",SwingConstants.CENTER); 
 		textLabel.setPreferredSize(new Dimension(300, 100));
 		
 		frame.getContentPane().add(textLabel, BorderLayout.CENTER);
